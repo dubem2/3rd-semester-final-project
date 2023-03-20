920aca6fa38bd5d5a70d36f075d68656f8c85648
George Owoh <60652826+dubem2@users.noreply.github.com>
2023-03-20 01:48:38 +0100

    initial commit

---
+++ jenkins-pipeline-to-deploy-to-eks/terraform/eks-cluster.tf
@@ -0,0 +1,33 @@
+module "eks" {
+    source  = "terraform-aws-modules/eks/aws"
+    version = "~> 19.0"
+    cluster_name = "myapp-eks-cluster"
+    cluster_version = "1.24"
+
+    cluster_endpoint_public_access  = true
+
+    vpc_id = module.myapp-vpc.vpc_id
+    subnet_ids = module.myapp-vpc.private_subnets
+
+    tags = {
+        environment = "development"
+        application = "myapp"
+    }
+
+    eks_managed_node_groups = {
+        node1 = {
+            min_size = 1
+            max_size = 3
+            desired_size = 2
+
+            instance_types = ["t2.small"]
+        }
+        node2 = {
+            min_size = 1
+            max_size = 3
+            desired_size = 2
+
+            instance_types = ["t2.small"]
+        }
+    }
+}
